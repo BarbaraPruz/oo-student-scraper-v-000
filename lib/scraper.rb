80580679493c52ff6a3fd37db17a01cd706927ab
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-17 19:55:37 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,31 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+
+    Nokogiri::HTML(open(index_url)).css('.student-card').each { | student |
+      students << {
+        :name => student.css(".card-text-container h4").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.css("a").attribute("href").value,
+      }
+    }
+    students
   end
 
+
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+##
+BASE_PATH = "./fixtures/student-site/"
+slist = Scraper.scrape_index_page(BASE_PATH + 'index.html')
+Student.create_from_collection(slist)
+Student.all.each { | student | { puts "#{student.name} #{student.location} #{student.profile_url}"}
